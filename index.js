2363e23e25a5a9646644df21ee392ab08a71376b
Jasson72 <145915046+Jasson72@users.noreply.github.com>
2025-02-20 22:09:43 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("img-fabio").addEventListener("click", function(){
+    alert("Halo");
+})
+document.getElementById("img-hari").addEventListener("click", function () {
+    alert("Kontol")
+})
